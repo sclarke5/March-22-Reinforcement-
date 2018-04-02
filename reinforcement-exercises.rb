f3ed7c6203f468ef19e303d04df97e7e2e095656
Sclarke5 <sclarke5555@gmail.com>
2018-04-02 19:32:20 -0400

    exercises 5-7

--- reinforcement-exercises.rb
+++ reinforcement-exercises.rb
@@ -23,13 +23,37 @@ direction_610 = array[2][:direction]
 p direction_610
 
 ####4$###
-north_trains = []
+northward_trains = []
 array.each do |train|
   if train[:direction]=="north"
-    north_trains.push(train[:train])
+    northward_trains.push(train[:train])
   end
 end
-p north_trains
+p northward_trains
 
 #####5#####
-east_trains = []
+eastward_trains = []
+array.each do |train|
+  if train[:direction]=="east"
+    eastward_trains.push(train[:train])
+  end
+end
+p eastward_trains
+
+######6######
+def train_direction(direction, list)
+direction_train = []
+list.each do |train|
+    if train[:direction] == direction
+      direction_train<<(train[:train])
+    end
+  end
+  return direction_train
+end
+
+p train_direction("north", array)
+p train_direction("east", array)
+
+#######7#######
+array[3][:departure_time] = 5
+p array[3]
